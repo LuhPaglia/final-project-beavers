e112d9840126daf15c78d4e8655247da6e5242b2
Honey Lee <honeyrun5@naver.com>
2023-01-05 01:26:56 -0800

    Get select data from database and send to client in JSON format

--- client/src/pages/Teacher.js
+++ client/src/pages/Teacher.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dashboard from "../components/Dashboard";
 import ModalCompo from "../components/ModalCompo";
 
@@ -28,7 +28,40 @@ const Teacher = () => {
     [2, 'pklimczak1', 'bdrover1@ox.ac.uk', 2, 1530, '05 Caliangt Street', '1965-9-26'],
     [3, 'rseamer2', 'gmarrow2@issuu.com', 3, null, 2374, '1965-9-26'],
     [4, 'ifealty3', 'csurmon3@princeton.edu', 4, null, '6617 Victoria Trail', '1965-9-26']
-  ]
+  ];
+
+  // const [select, setSelect] = useState(null);
+
+  // async function fetchUserData(id) {
+  //   const response = await fetch("/" + id);
+  //   setSelect(await response.json());
+  // }
+
+  
+
+  // const [select, setSelect] = useState
+
+  function selectFromDatabase() {
+    fetch('http://localhost/final-project-beavers/server/teacherSelect.php').then(response => {
+        console.log(response);
+        return response.json();
+      }).then(result => {
+        // Work with JSON data here
+        console.log(result);
+
+        // setSelect({
+        //     employee_r:result
+        // }); 
+
+        // setSelect({
+        //   result.map((row) => Object.values(row))
+        // });
+
+      }).catch(err => {
+        // Do something for an error here
+        console.log("Error Reading data " + err);
+      });
+  }
 
   return (
     <>
@@ -36,6 +69,11 @@ const Teacher = () => {
         <Row>
           <Col>
             <h1>Teacher Management</h1>
+
+            <Button variant="success" onClick={selectFromDatabase}>
+              select
+            </Button>
+
           </Col>
           <Col>
             <Button variant="success" onClick={handleShow}>
