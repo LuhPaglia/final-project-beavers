edaf1571df9ab677af23ca29fd12d6ec37908a09
Honey Lee <honeyrun5@naver.com>
2023-01-06 18:55:09 -0800

    git ignore update

--- server/data/config.php
+++ server/data/config.php
@@ -1,9 +1,8 @@
 <?php
 
-    header("Access-Control-Allow-Origin: *");
-    // header("Access-Control-Allow-Headers: access");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
     header("Access-Control-Allow-Methods: POST");
-    header("Content-Type: application/json; charset=UTF-8");
+    // header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
     $baseName = "http://localhost/final-project-beavers/server/";
--- server/teacherAdd.php
+++ server/teacherAdd.php
@@ -21,20 +21,17 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
     $newTeacher = new teacherObj(null,$user_name,$password,$email,$course_id,$salary,$address,$birthday);
     $valuesArray = $newTeacher->toInsert();
     $fieldArray = ["user_name","password","email","course_id","salary","address", "birthday"];
-    print_r($valuesArray); // Array ( [0] => 31 [1] => fullname [2] => asdf [3] => asdf@google.com [4] => 3 [5] => 3000 [6] => 12 Qwe Asdxc [7] => 1965-9-26 )
+    print_r($valuesArray);
 
     if($dbSrv->dbConnect()){
         if($dbSrv->insert('teacher_tb',$valuesArray,$fieldArray)){
-            // print_r();
-            // "added";
             // header("Location: http://localhost:3000/teacher");
             // exit();
         }
     } else{
         echo "DB connection problem";
     }
-    // "not added";
-
+    $dbSrv->closeDb();
 }
 
 ?>
