b80cc6396f9cbc468831d6770f03aaf0e2d9b618
moca5665 <moca5665@gmail.com>
2023-01-13 22:43:40 -0800

    styled

--- src/pages/StudentProfile.js
+++ src/pages/StudentProfile.js
@@ -1,33 +1,34 @@
-import '../css/StudentProfile.css';
-import profile from '../'
+import {StyledStudentProfile} from "../styles";
+
 
 const Student = () => {
   return (
-    
-    <div>
-      <section class="profile">
+    <StyledStudentProfile>
+      <div class="studentProfile">
+        <section class="profile">
+          
+          <aside>
+          <img src="https://images.unsplash.com/photo-1628563694622-5a76957fd09c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHw%3D&w=1000&q=80"></img>
+          <table>
+            <tbody>
+              <tr>
+                <td><b>Id:</b> 0</td>
+                <td><b>Name:</b> Daron Mabee</td>
+                <td><b>Date of Birth:</b> 28-11-2006</td>
+                <td><b>Address:</b> Suite 99</td>
+                <td><b>Phone:</b> 399-273-1768</td>
+                <td><b>Course:</b> HTML</td>
+              </tr>
+            </tbody>
+          </table>
+          </aside>
+          
+        </section>
         
-        <aside>
-        <img src="https://images.unsplash.com/photo-1628563694622-5a76957fd09c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHw%3D&w=1000&q=80"></img>
-        <table>
-          <tbody>
-            <tr>
-              <td><b>Id:</b> 0</td>
-              <td><b>Name:</b> Daron Mabee</td>
-              <td><b>Date of Birth:</b> 28-11-2006</td>
-              <td><b>Address:</b> Suite 99</td>
-              <td><b>Phone:</b> 399-273-1768</td>
-              <td><b>Course:</b> HTML</td>
-            </tr>
-          </tbody>
-        </table>
-        </aside>
+        <footer></footer>
         
-      </section>
-      
-      <footer></footer>
-      
-    </div>
+      </div>
+    </StyledStudentProfile>
   )
 }
 
