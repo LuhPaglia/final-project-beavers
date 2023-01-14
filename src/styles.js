4f16a64822cd76444b10ec954c71c3e0a4f42660
RickyTheJenius <117127600+RickyTheJenius@users.noreply.github.com>
2023-01-13 17:05:12 -0800

    Update styles.js
    changed

--- src/styles.js
+++ src/styles.js
@@ -1,59 +1,38 @@
-import styled from 'styled-components'
-
-export const StyledLogin = styled.div`
-  h1 {
-    font-size: 28px;
-    color: #333;
-    padding-right:23vh;
-  }
-  padding-left: 15vh;
-  padding-top: 5vh;
-  
-  .container {
-    padding-top: 20px;
-    padding-bottom:
-    20px;
-    width: 50%;
-  }
-
-  form {
-    padding-right: 0;
-    border-radius: 4px;
-    padding: 3vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    fieldset {
-      legend {
-        font-family: 'Sans-serif';
-        font-size: 18px;
-        color: #333;
-      }
+export const styles = {
+  green: {color:'#198722'},
+  white: {color:'#198722', fontWeight:'700'},
+  bgHover: {
+    width: '100%',
+    fontFamily:'Sans-serif',
+    fontSize: '16px',
+    background: 'transparent',
+    border: '1px solid #198722',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: '4px',
+    color:'#198722'
+  },
+  bgEnd: {
+    width: '100%',
+    fontFamily:'Sans-serif',
+    fontSize: '16px',
+    background: 'linear-gradient(to right, #198722, #5DE168)',
+    border: 'none',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: '4px'
+  },
+  bgCh: (e) =>{
+    switch(e.type){
+        case "focus":
+            e.target.style.boxShadow="0 0 0 3px #198722";
+            e.target.style.border="none";
+        break;
+        case "blur":
+          e.target.style.boxShadow="none";
+          e.target.style.border="1px solid #ced4da";
+        break;
     }
-
-    .form-check {
-      font-family: 'Sans-serif';
-      font-size: 16px;
-      color: #333;
-    }
-
-    .form-control {
-      font-family: 'Sans-serif';
-      font-size: 16px;
-      color: #333;
-    }
-
-    .btn {
-      width: 100%;
-      font-family: 'Sans-serif';
-      font-size: 16px;
-      background: linear-gradient(to right, #0077c9, #00b4db);
-      border: none;
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-      border-radius: 4px;
-      color: white;
-      padding: 12px 24px;
-    }
-  }
-`;
+  },
+  color: { marginRight: '8px', color:'#198722'},
+  font: { fontFamily: 'Sans-serif', fontSize: '16px', color: '#333'},
+  fieldset: { display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center'}
+}
