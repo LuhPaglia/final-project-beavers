e1f57756127fdcb986d63d57cae533e5d535bd3c
moca5665 <moca5665@gmail.com>
2023-01-15 20:12:01 -0800

    fisrt

---
+++ server/services/grade/gradeSelect.php
@@ -0,0 +1,34 @@
+<?php
+  include '../../services/dbservices.php';
+  
+  if ($_SERVER["REQUEST_METHOD"] == 'GET') {
+
+    $dbSrv = new dbServices($hostName,$userName,$password,$dbName);
+
+    if($dbcon = $dbSrv->dbConnect()){
+      
+      $sqlCommand = "SELECT `grade_id`, `classwork_name`, `evaluation`, `student_id`, `teacher_id`, `course_id`, `mark`, `mark_max`, `mark_date`, `feedback`, `status` FROM `grade_tb`WHERE grade_tb.status = 1";
+      
+      
+      // run SQL statement
+      $result = $dbcon->query($sqlCommand);
+  
+      // die if SQL statement failed...??
+      if (!$result) {
+        http_response_code(404);
+        die(mysqli_error($con));
+      }
+
+      $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+      // return query result
+      echo json_encode($rows);
+  
+    }else{
+      echo "DB connection problem";
+    }
+
+    $dbSrv->closeDb();
+  }
+
+?>
